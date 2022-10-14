0a2c3fc6714375094389a95b2e12e5e18070be15
Christian Villegas <chrisvill.2312@gmai.com>
2022-10-14 03:50:45 +0000

    Created API Template

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello!");
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+
+const startServer = () => {
+  server.listen(80, () => {
+    console.log("Listening at 80");
+  });
+};
+
+const startDB = async () => {
+  return await conn.sync({ force: true });
+};
+
+const start = async () => {
+  await startDB();
+  startServer();
+}
+
+start();
