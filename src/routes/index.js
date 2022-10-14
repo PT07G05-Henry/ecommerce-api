7ae67efa922e4d61ec66ae31cbc2b689310333dd
Ramiro Alfaro <ralfaro@canal2.local>
2022-10-14 20:15:39 -0300

    primer Get

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
 const { Router } = require("express");
+const {User, Product, Payment, Delivery, Categories, Products_Categories, Order_Products, Order} = require("../db")
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -6,6 +8,19 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+const consulproduct = async () => {
+  return await Product.findAll({
+      include: {
+          model: Products_Categories,
+          attributes: ["category_ID"],
+        through: {
+              attributes: [],
+          }, 
+      }
+  })
+
+} 
 
 
 router.get("/test", async (req, res) => {
@@ -16,6 +31,27 @@ router.all("*", (req, res) => {
   res.redirect("/");
 });
 
+router.get('/home', async(req, res) => {
+  const product_name  = req.query.productname
+  let articulo = await consulproduct();
+  
+ 
+  if (product_name) {
+
+      let buscararticulo = await articulo.filter(e => e.product_name.toLowerCase().includes(product_name.toLowerCase()))
+      buscararticulo.length ? res.status(200).send(buscararticulo) : res.status(404).send("No existe articulo")
+  }
+  
+  else {
+      return res.status(200).send(buscararticulo)
+
+
+  }
+
+   })   
+
 router;
+
+
 
 module.exports = router;
