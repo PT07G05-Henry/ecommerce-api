031b0989e7461d29b6398c1d4fa68fa45f21c855
francoSerafini <frankitooserafini493@gmail.com>
2022-10-14 16:08:26 -0300

    Se agrego archivo Delivery.js a models con sus atributos

---
+++ src/models/Delivery.js
@@ -0,0 +1,49 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una función que define el modelo
+// Luego le inyectamos la conexión a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("delivery", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [
+          [
+            "Home Delivery",
+            "Shipping to a delivery ponint",
+            "Express shipping.",
+            "Standard Shipping",
+            "International Shipping",
+          ],
+        ],
+      },
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [["On the way", "Pre-admission", "Delivered"]],
+      },
+    },
+    shipping_address: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    phone_number: {
+      type: DataTypes.STRING,
+      validate: {
+        isPhoneNumber(value) {
+          if (/\d{3}-\d{3}-\d{4}/.test(value)) {
+            throw new Error("Invalid format");
+          }
+        },
+      },
+    },
+  });
+};
