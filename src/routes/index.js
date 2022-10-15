07cbe8c66a8f1e6244d6045153dd041059010af8
mativillagra98 <mativillagra98@gmail.com>
2022-10-15 13:38:50 -0300

    Ruta para obtener el detalle de algun prodcuto con sus categorias

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { Product, Category } = require('../db')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,15 +8,33 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+const getDetailProduct = async (id) => {
+  return await Product.findByPk( id ,{
+    include: {
+      model: Category,
+      attributes: ["name"],
+      through: {
+        attributes: [],
+      }, 
+    }
+  })
+}
 
 router.get("/test", async (req, res) => {
   res.status(200).send({ hi: "Hello world!!!" })
+});
+
+router.get("/product/:id", async (req, res) => {
+  let id = req.params.id;
+  let product = await getDetailProduct(id);
+  product
+    ? res.status(200).send(product)
+    : res.status(404).send({ error: "Product Not Found" });
 });
 
 router.all("*", (req, res) => {
   res.redirect("/");
 });
 
-router;
 
 module.exports = router;
