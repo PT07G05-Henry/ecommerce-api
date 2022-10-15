bc53ed57b4ad79218f61550314cd9935a99b78f4
Profesorjand <jorge.andrade.y@hotmail.com>
2022-10-15 02:58:38 -0300

    Feat/Creacion del Modelo Rol

---
+++ src/models/Rol.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una función que define el modelo
+// Luego le inyectamos la conexión a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("rol", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    type:{
+        type: DataTypes.ENUM(["User", "Admin"]),
+        allowNull: false,
+    }
+  },{
+    timestamps: false,
+  });
+};
