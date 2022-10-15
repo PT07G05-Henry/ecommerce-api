0d2662c6fbec19622ffc7f384370a97fe613fb78
FEDEIZ <ing.finterlandi@gmail.com>
2022-10-15 15:56:00 -0300

    Agregando carga de datos de modelos faltantes.

--- src/loadDb.js
+++ src/loadDb.js
@@ -1,5 +1,13 @@
 const axios = require("axios");
-const { User, Product } = require("./db.js");
+const {
+  User,
+  Product,
+  Payment,
+  Order,
+  Delivery,
+  Comment,
+  Category,
+} = require("./db.js");
 require("dotenv").config();
 const { MOCKAPI, APIKEY } = process.env;
 
@@ -30,7 +38,53 @@ module.exports = async () => {
         rating: p.rating,
       });
     });
+    const payments = await axios.get(`${MOCKAPI}/payment.json?key=${APIKEY}`);
+    await payments.data.forEach((p) => {
+      Payment.upsert({
+        id: p.id,
+        type: p.type,
+        status: p.status,
+      });
+    });
 
+    const orders = await axios.get(`${MOCKAPI}/order.json?key=${APIKEY}`);
+    await orders.data.forEach((o) => {
+      Order.upsert({
+        id: o.id,
+        total_price: o.total_price,
+        status: o.status,
+      });
+    });
+
+    const deliverys = await axios.get(`${MOCKAPI}/delivery.json?key=${APIKEY}`);
+    await deliverys.data.forEach((d) => {
+      Delivery.upsert({
+        id: d.id,
+        type: d.type,
+        status: d.status,
+        shipping_address: d.shipping_address,
+        phone_number: d.phone_number,
+      });
+    });
+
+    const comments = await axios.get(`${MOCKAPI}/comment.json?key=${APIKEY}`);
+    await comments.data.forEach((c) => {
+      Comment.upsert({
+        id: c.id,
+        value: c.value,
+      });
+    });
+
+    const categories = await axios.get(
+      `${MOCKAPI}/category.json?key=${APIKEY}`
+    );
+    await categories.data.forEach((c) => {
+      Category.upsert({
+        id: c.id,
+        name: c.name,
+        image: c.image,
+      });
+    });
     console.log("DB LOADED SUCCESSFULLY!");
     return;
   } catch (err) {
