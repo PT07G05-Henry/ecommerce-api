2841054993e125c2eb479261d55ac1a0bf82e800
FEDEIZ <ing.finterlandi@gmail.com>
2022-10-16 23:07:12 -0300

    filtradoBack

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { Product, Category, Order, Comment } = require('../db')
+const { Product, Category, Order, Comment } = require("../db");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -20,15 +20,15 @@ const getProductsByFilter = async (name) => {
 };
 
 const getDetailProduct = async (id) => {
-  return await Product.findByPk( id ,{
+  return await Product.findByPk(id, {
     include: {
       model: Category,
       attributes: ["name"],
       through: {
         attributes: [],
-      }, 
-    }
-  })
+      },
+    },
+  });
 };
 
 const getAllProducts = async function () {
@@ -70,6 +70,55 @@ router.get("/:id", async (req, res) => {
     : res.status(404).send({ error: "Product Not Found" });
 });
 
+//filtrado desde back!
+/*
+/products
+?page=numeroDePagina (1 a totalPages)
+?quantity=cantidadDeProductos       //establezcan una cantidad por defecto en caso de no mandarlo
+?category=IdDeCategoria
+?orderbyname=true
+?orderby<lo que se les ocurra>=true
+?serachbyname=nombre 
+Se tienen que poder mezclar los query en el mismo endpoint
+*/
+
+const getAllProducts2 = async function (page, quantity) {
+  let productsPerPage = await Product.findAndCountAll({
+    limit: quantity,
+    offset: (page - 1) * quantity,
+  });
+  return productsPerPage;
+};
+router.get("/", async (req, res) => {
+  let {
+    name,
+    page = 1,
+    quantity = 10,
+    category,
+    orderByName,
+    orderByPrice,
+    orderByRating,
+  } = req.query;
+
+  try {
+    let products;
+    if (name) {
+      products = await getProductsByFilter(name);
+      if (products.length === 0)
+        return res.send("There are no matches in the DB").status(404);
+      // res.status(200).send(products);
+    } else {
+      products = await getAllProducts2(page, quantity);
+      if (products.length === 0)
+        return res.send("There are no products loaded in the DB");
+      // res.status(200).send(products);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+});
+
 router.post("/", async function (req, res) {
   const { name, price, description, stock, images } = req.body;
   if (!name || !price || !description || !stock || !images) {
@@ -89,22 +138,24 @@ router.post("/", async function (req, res) {
 router.put("/", async (req, res) => {
   const { id, name, price, description, stock, images } = req.body;
   try {
-  await Product.update({ 
-    name: name,
-    price: price,
-    description: description,
-    stock: stock,
-    images: images
-  }, 
-  {
-    where: {
-      id: id
-    }
-  });
-  let productUpdate = await getDetailProduct(id)
-  res.status(200);
-  res.send(productUpdate);
-  } catch(err) {
+    await Product.update(
+      {
+        name: name,
+        price: price,
+        description: description,
+        stock: stock,
+        images: images,
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+    let productUpdate = await getDetailProduct(id);
+    res.status(200);
+    res.send(productUpdate);
+  } catch (err) {
     res.status(400);
     res.send(err.message);
   }
@@ -115,15 +166,15 @@ router.delete("/", async (req, res) => {
   try {
     await Product.destroy({
       where: {
-        id: id
-      }
+        id: id,
+      },
     });
     res.status(200);
-    res.send('Product Removed Successfully')
+    res.send("Product Removed Successfully");
   } catch (err) {
     res.status(400);
-    res.send(err.message)
+    res.send(err.message);
   }
 });
 
-  module.exports = router;
+module.exports = router;
