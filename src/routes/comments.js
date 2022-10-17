6d65c2dd97c50a2bb75ed4d5d371db88800eecd3
francoSerafini <frankitooserafini493@gmail.com>
2022-10-17 18:14:20 -0300

    CRUD completo ruta commets

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -1,34 +1,102 @@
 const { Router } = require("express");
-const { Comment } = require('../db');
+const { Comment } = require("../db");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 const router = Router();
 
 // Configura las funciones
-
+const getCommentsById = async (id, relation) => {
+  if (!relation) return Comment.findByPk(id);
+  else if (relation.toLowerCase() === "product") {
+    return Comment.findAll({
+      where: {
+        productId: id,
+      },
+    });
+  } else if (relation.toLowerCase() === "user") {
+    return Comment.findAll({
+      where: {
+        userId: id,
+      },
+    });
+  }
+};
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.get("/", async(req, res)=>{
+router.get("/:id", async (req, res) => {
+  let { id } = req.params;
+  let { relation } = req.query;
+  console.log(relation, id);
+  try {
+    comments = await getCommentsById(id, relation);
+    if (comments.length === 0)
+      return res.send("There are no matchs in the DB").status(404);
+    res.status(200).send(comments);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
     const comments = await Comment.findAll();
-    comments
-      ? res.status(200).send(comments)
-      : res.status(404).send({ error: "Comment Not Found" });
+    if (comments.length === 0)
+      return res.send("There are no comments loaded in the DB").status(404);
+    res.status(200).send(comments);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 router.post("/", async (req, res) => {
-    //falta escribir acá
+  const { value, userId, productId } = req.body;
+  if (!value) return res.status(400).send("Missing to send mandatory data");
+  try {
+    let newComment = await Comment.create(req.body);
+    await newComment.setProduct(productId);
+    await newComment.setUser(userId);
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(400);
+    res.send(err.message);
+  }
 });
 
 router.put("/", async (req, res) => {
-    //falta escribir acá
+  const { id, value } = req.body;
+  try {
+    await Comment.update(
+      { value },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+    res.status(200).send("Comment uploaded successfully");
+  } catch (err) {
+    res.status(400);
+    res.send(err.message);
+  }
 });
 
 router.delete("/", async (req, res) => {
-    //falta escribir acá
+  const { id } = req.body;
+  try {
+    await Comment.destroy({
+      where: {
+        id: id,
+      },
+    });
+    res.status(200);
+    res.send("Comment removed succesfully");
+  } catch (err) {
+    res.status(400);
+    res.send(err.message);
+  }
 });
-
 
 module.exports = router;
