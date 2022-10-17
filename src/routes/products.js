93d54ab84c6604defe7265c11719d3a9176b410f
FEDEIZ <ing.finterlandi@gmail.com>
2022-10-17 19:53:11 -0300

    readyToReceiveText

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
-const { Product, Category, Order, Comment, Op} = require("../db");
+const { Product, Category, Order, Comment, Op } = require("../db");
 require("dotenv").config();
-const {PORT} = process.env
+const { PORT } = process.env;
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -9,35 +9,45 @@ const router = Router();
 
 // Configura las funciones
 
-const getAllProducts2 = async function (page, quantity, order="id", typeOrder="ASC", category, name) {
+const getAllProducts2 = async function (
+  page,
+  quantity,
+  order = "id",
+  typeOrder = "ASC",
+  category,
+  name
+) {
   const pageAsNumber = Number.parseInt(page);
-  const quantityAsNumber = Number.parseInt(quantity)
+  const quantityAsNumber = Number.parseInt(quantity);
 
   let pag = 1;
-  if(!Number.isNaN(pageAsNumber) && pageAsNumber > 1){
+  if (!Number.isNaN(pageAsNumber) && pageAsNumber > 1) {
     pag = pageAsNumber;
   }
   let quant = 10;
-  if(!Number.isNaN(quantityAsNumber) && quantityAsNumber > 0 && quantityAsNumber < 20){
+  if (
+    !Number.isNaN(quantityAsNumber) &&
+    quantityAsNumber > 0 &&
+    quantityAsNumber < 20
+  ) {
     quant = quantityAsNumber;
   }
   let productsPerPage = await Product.findAndCountAll({
     limit: quantity,
     offset: (page - 1) * quantity,
-    order: [ [order?order.toLowerCase():"id", typeOrder.toUpperCase()] ],
+    order: [[order ? order.toLowerCase() : "id", typeOrder.toUpperCase()]],
     distinct: true, // no eliminar esto, ya que la funcion findAndCountAll se descontrola
-    include: category ? [
-      {
-        model:Category,
-        where:{id: category}
-      },
-      Comment,
-      Order
-    ]:
-    [Comment,
-      Order,
-      Category],
-    where: name?{name: {[Op.iLike]: `%${name}%`}}:{}
+    include: category
+      ? [
+          {
+            model: Category,
+            where: { id: category },
+          },
+          Comment,
+          Order,
+        ]
+      : [Comment, Order, Category],
+    where: name ? { name: { [Op.iLike]: `%${name}%` } } : {},
   });
   return productsPerPage;
 };
@@ -74,7 +84,6 @@ const getAllProducts = async function () {
   return products;
 };
 
-
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.get("/", async (req, res) => {
@@ -88,37 +97,57 @@ router.get("/", async (req, res) => {
   } = req.query;
 
   try {
-    let products = await getAllProducts2(page, quantity, orderBy, typeOrder, category, name);
+    let products = await getAllProducts2(
+      page,
+      quantity,
+      orderBy,
+      typeOrder,
+      category,
+      name
+    );
     if (products.length === 0)
       return res.send("There are no products loaded in the DB");
-      const protocol = req.protocol;
-      const host = req.hostname;
-      const url = req.originalUrl;
-      const port = process.env.PORT || PORT;
-      const query = req.query;
+    const protocol = req.protocol;
+    const host = req.hostname;
+    const url = req.originalUrl;
+    const port = process.env.PORT || PORT;
+    const query = req.query;
 
-      const pagNext = Object.keys(query).length===0?
-      `${protocol}://${host}:${port}${url.includes("?")?url.concat("&page=2", "&quantity="+Number(quantity)):url.concat("?page=2", "&quantity="+Number(quantity))}`:
-      url.includes("page=")?
-      `${protocol}://${host}:${port}${url.replace(/page=\d{1,}/g, `page=${Number(page)+1}`)}`:
-      `${protocol}://${host}:${port}${url.concat("&page=2")}`;
+    const pagNext =
+      Object.keys(query).length === 0
+        ? `${protocol}://${host}:${port}${
+            url.includes("?")
+              ? url.concat("&page=2", "&quantity=" + Number(quantity))
+              : url.concat("?page=2", "&quantity=" + Number(quantity))
+          }`
+        : url.includes("page=")
+        ? `${protocol}://${host}:${port}${url.replace(
+            /page=\d{1,}/g,
+            `page=${Number(page) + 1}`
+          )}`
+        : `${protocol}://${host}:${port}${url.concat("&page=2")}`;
 
-      const pagPrev = url.includes("page=")?
-      `${protocol}://${host}:${port}${url.replace(/page=\d{1,}/g, `page=${Number(page)-1}`)}`:
-      `${protocol}://${host}:${port}${url.concat("&page="+Number(page)-1)}`;
+    const pagPrev = url.includes("page=")
+      ? `${protocol}://${host}:${port}${url.replace(
+          /page=\d{1,}/g,
+          `page=${Number(page) - 1}`
+        )}`
+      : `${protocol}://${host}:${port}${url.concat(
+          "&page=" + Number(page) - 1
+        )}`;
 
-      const obj = {
-        totalProducts: products.count,
-        next: (Number(page)+1)>Math.ceil(products.count/quantity)?"":pagNext,
-        prev: (Number(page)-1)===0?"":pagPrev,
-        totalPage: Math.ceil(products.count/quantity),
-        page: page,
-        quantity: quantity,
-        results: products.rows, 
-      }
-      res.status(200).send(obj);
-    }
-   catch (err) {
+    const obj = {
+      totalProducts: products.count,
+      next:
+        Number(page) + 1 > Math.ceil(products.count / quantity) ? "" : pagNext,
+      prev: Number(page) - 1 === 0 ? "" : pagPrev,
+      totalPage: Math.ceil(products.count / quantity),
+      page: page,
+      quantity: quantity,
+      results: products.rows,
+    };
+    res.status(200).send(obj);
+  } catch (err) {
     console.log(err);
     res.status(404).send(err.message);
   }
@@ -133,14 +162,24 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async function (req, res) {
-  const { name, price, description, stock, images } = req.body;
-  if (!name || !price || !description || !stock || !images) {
+  const { name, price, description, stock, images, categories } = req.body;
+
+  if (!name || !price || !description || !stock || !images.length) {
     res.status(400);
     return res.send("Missing to send mandatory data");
   }
   try {
-    let newProduct = await Product.create(req.body);
-    await newProduct.setCategories(req.body.categories);
+    let newProduct = await Product.create({
+      name,
+      price: Number.parseFloat(price.replace(",", ".")).toFixed(2),
+      description,
+      stock: Number.parseInt(stock),
+      images,
+    });
+
+    categories.forEach((c) => Number.parseInt(c));
+
+    await newProduct.setCategories(categories);
     res.sendStatus(201);
   } catch (err) {
     res.status(400);
