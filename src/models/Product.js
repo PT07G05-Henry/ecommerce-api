be098f6e20eb586593ab114757fe0df5ba91f73c
Profesorjand <jorge.andrade.y@hotmail.com>
2022-10-18 01:29:54 -0300

    ahora si

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,4 +1,6 @@
+const { constants } = require("crypto");
 const { DataTypes } = require("sequelize");
+const isImgUrl = require("../functions/isImgUrl");
 // Exportamos una función que define el modelo
 // Luego le inyectamos la conexión a sequelize.
 module.exports = (sequelize) => {
@@ -32,16 +34,31 @@ module.exports = (sequelize) => {
       },
       images: {
         type: DataTypes.TEXT,
-        allowNull: false,
-        get: function() {
-          return JSON.parse(this.getDataValue('images'));
-        }, 
-        set: function(val) {
-            if(val.length === 0){
-              return this.setDataValue('images',JSON.stringify([{"image":"https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"}]));
-            }
-            return this.setDataValue('images', JSON.stringify(val));
+        // allowNull: false,
+        get: function () {
+          return JSON.parse(this.getDataValue("images"));
         },
+        set: function (val) {
+          if(val.length === 0){             
+            return this.setDataValue('images',JSON.stringify([{"image":"https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"}]));
+          }
+          return this.setDataValue("images", JSON.stringify(val));
+        },
+        validate: {
+          async customValidator(value) {
+            for (const img in value) {
+              if (!(await isImgUrl(img.image))) {
+                throw new Error("Image url is broken");
+              }
+            }
+          },
+        },
+        defaultValue: JSON.stringify([
+          {
+            image:
+              "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg",
+          },
+        ]),
       },
       rating: {
         type: DataTypes.INTEGER,
@@ -49,8 +66,8 @@ module.exports = (sequelize) => {
           max: 5,
           min: 0,
         },
-        defaultValue:0,
-      }, 
+        defaultValue: 0,
+      },
       // comments: {
       //   type: DataTypes.ARRAY(DataTypes.TEXT),
       // },
