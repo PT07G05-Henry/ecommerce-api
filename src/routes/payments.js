8f81cef7ce3296521e48fd5b3385f102c23d1cc6
francoSerafini <frankitooserafini493@gmail.com>
2022-10-18 18:03:47 -0300

    CRUD en ruta payments

--- src/routes/payments.js
+++ src/routes/payments.js
@@ -7,28 +7,71 @@ const router = Router();
 
 // Configura las funciones
 
-
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
 router.get("/", async(req, res)=>{
-    const payments = await Payment.findAll();
-    payments
-      ? res.status(200).send(payments)
-      : res.status(404).send({ error: "Payment Not Found" });
+    try {
+        const payments = await Payment.findAll();
+    if (payments.length === 0) return res.send("There are no payments loaded on the DB");
+    res.status(200).send(payments);
+    } catch {
+        res.status(500).send(err.message);
+    }
 });
 
 router.post("/", async (req, res) => {
-    //falta escribir acá
+    const { type, status } = req.body;
+  if (!type || !status) {
+    res.status(400);
+    return res.send("Missing to send mandatory data");
+  }
+  try {
+    let newPayment = await Payment.create(req.body);
+    await newPayment.setOrder(req.body.order);
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(400);
+    res.send(err.message);
+  }
 });
 
 router.put("/", async (req, res) => {
-    //falta escribir acá
+    const { id, type, status} = req.body;
+    try {
+      await Payment.update(
+        {
+         type: type,
+         status: status 
+        },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+      res.status(200).send("Payment uploaded successfully");
+    } catch (err) {
+      res.status(400);
+      res.send(err.message);
+    }
 });
 
 router.delete("/", async (req, res) => {
-    //falta escribir acá
-});
+    const { id } = req.body;
+    try {
+      await Payment.destroy({
+        where: {
+          id: id,
+        },
+      });
+      res.status(200);
+      res.send("Payment Removed Successfully");
+    } catch (err) {
+      res.status(400);
+      res.send(err.message);
+    }
+  });
 
 
 module.exports = router;
