bac6981d656b79f94ee84778c456a0b5bc5e97c8
FEDEIZ <ing.finterlandi@gmail.com>
2022-10-28 16:15:18 -0300

    Verifico dueño del comentario

--- src/routes/middlewares/owner.js
+++ src/routes/middlewares/owner.js
@@ -1,9 +1,10 @@
 const { User, Users_rols, Comments, Order } = require("../../db");
 const { isProductOwner } = require("../auxFunctions/productOwner");
+const { isCommentOwner } = require("../auxFunctions/commentOwner");
 const isOwner = async (req, res, next) => {
   // verify if the user is owner of what he want to update or delete
   const { sid } = req.query;
-  const { id_item, item_type } = req.body;
+  const { itemType, itemId } = req.body;
   //console.log("sid", sid);
   try {
     const userDb = await User.findOne({ where: { sid } });
@@ -21,18 +22,19 @@ const isOwner = async (req, res, next) => {
       switch (itemType) {
         case "PRODUCT":
           console.log("product owner?");
-          if (await isProductOwner(idItem, user_rol.dataValues.id)) {
+          if (await isProductOwner(itemId, user_rol.dataValues.id)) {
             //console.log("is owner");
             return next();
           }
           break;
         case "COMMENT":
           console.log("comment owner?");
+          if (await isCommentOwner(itemId, userDb.dataValues.id)) {
+            console.log("is owner");
+            return next();
+          }
           break;
 
-        case "ORDER":
-          console.log("order owner?");
-          break;
         default:
           break;
       }
