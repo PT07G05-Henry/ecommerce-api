ad58edb746f94b40241b99a78371cd0f1411273d
FEDEIZ <ing.finterlandi@gmail.com>
2022-10-28 11:27:12 -0300

    All orders for superAdmin dashboard

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -4,15 +4,21 @@ const { deleteOrder } = require("./controllers/orders/deleteOrder");
 const { getOrderDetail } = require("./controllers/orders/getOrderDetail");
 const { getOrders } = require("./controllers/orders/getOrders");
 const { updateOrder } = require("./controllers/orders/updateOrder");
+const { getAllOrders } = require("./controllers/orders/getAllOrders");
 
 //Middlewares
 const { isAuthenticated } = require("./middlewares/auth");
 const { isUser } = require("./middlewares/user");
 const { isOwner } = require("./middlewares/owner");
+const { isSuperAdmin } = require("./middlewares/superAdmin");
 
 const router = Router();
 
+// For admin or user dashboard
 router.get("/", isAuthenticated, getOrders);
+
+// Just for superadmin
+router.get("/all", isAuthenticated, isSuperAdmin, getAllOrders);
 
 router.get("/:id", isAuthenticated, getOrderDetail);
 
