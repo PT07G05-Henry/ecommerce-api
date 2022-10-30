f359502b4001017198f23e86597a08ce4de67c8e
Profesorjand <jorge.andrade.y@hotmail.com>
2022-10-29 22:35:24 -0300

    cambiar tipo por type

--- src/routes/controllers/email/sendEmail.js
+++ src/routes/controllers/email/sendEmail.js
@@ -1,60 +1,80 @@
 require("dotenv").config();
 const { GMAIL_PASSWORD } = process.env;
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 const htmlTesting = require("./templates/htmlTesting");
 const htmlNewUserEmail = require("./templates/newUserEmail");
-const htmlNewBuyCart = require("./templates/newBuyCart")
+const htmlNewBuyCart = require("./templates/newBuyCart");
 
-const emailExample = async (req, res) => {
-    const {email, asunto, mensaje, tipo, products, directionAddress, totalPrice} = req.body; //tipo es para saber que tipo de correo enviar
- nodemailer.createTestAccount((err,account)=>{
+const sendEmail = async (req, res) => {
+  const {
+    email,
+    subject,
+    message,
+    type,
+    products,
+    directionAddress,
+    totalPrice,
+  } = req.body; //type es para saber que type de correo enviar
+  if (!email || subject || message)
+    return res.status(404).send("Data missing (Email, Subject Or Message)");
+  nodemailer.createTestAccount((err, account) => {
     var htmlEmail = `
     <html>
     <body>
     <h3>Email Enviado</h3>
     <ul>
         <li>Email: ${email}</li>
-        <li>Asunto: ${asunto}</li>
+        <li>Asunto: ${subject}</li>
     </ul>
     <h3>Mensaje</h3>
-    <p>${mensaje}</p>
+    <p>${message}</p>
     </body>
     </html>
     `;
-    if(tipo === "newUserEmail"){
-        htmlEmail = htmlNewUserEmail(email, asunto, mensaje)
+    if (type === "newUserEmail") {
+      htmlEmail = htmlNewUserEmail(email, subject, message);
     }
-    if(tipo === "newBuyCart"){
-        htmlEmail = htmlNewBuyCart(email, asunto, mensaje, products, directionAddress, totalPrice)
+    if (type === "newBuyCart") {
+      if (!products.length || !directionAddress || !totalPrice)
+        return res
+          .send(404)
+          .send("Data missing (products, directionAddress or totalPrice)");
+      htmlEmail = htmlNewBuyCart(
+        email,
+        subject,
+        message,
+        products,
+        directionAddress,
+        totalPrice
+      );
     }
-    //const htmlPrueba = htmlTesting(email, asunto, mensaje);
+    //const htmlPrueba = htmlTesting(email, subject, message);
     let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        auth: {
-            user: "techmerchpt@gmail.com", //el email del servicio SMTP que a utilizar
-            pass: GMAIL_PASSWORD //contraseña protegida por .env del email de arriba
-        }
+      host: "smtp.gmail.com",
+      port: 587,
+      auth: {
+        user: "techmerchpt@gmail.com", //el email del servicio SMTP que a utilizar
+        pass: GMAIL_PASSWORD, //contraseña protegida por .env del email de arriba
+      },
     });
     let mailOptions = {
-        from: "techmerchpt@gmail.com", //quien manda el email
-        to: req.body.email, // el email destino
-        replyTo: "techmerchpt@gmail.com",
-        subject: req.body.asunto, //El asunto del email
-        text: req.body.mensaje, //el mensaje
-        html: htmlEmail // La parte html del email
+      from: "techmerchpt@gmail.com", //quien manda el email
+      to: email, // el email destino
+      replyTo: "techmerchpt@gmail.com",
+      subject: subject, //El subject del email
+      text: message, //el message
+      html: htmlEmail, // La parte html del email
     };
-    transporter.sendMail(mailOptions, (err,info) =>{
-        if(err){
-            console.log(err)
-            return res.status(404).send("Hubo un Error");
-        }
-        console.log("Mensaje enviado: %s", info.mensaje);
-        console.log("Url del mensaje: %s", nodemailer.getTestMessageUrl(info));
-        return res.status(200).send("Enviado")
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log(err);
+        return res.status(404).send("Error sending Email");
+      }
+      console.log("Mensaje enviado: %s", info.accepted);
+      console.log("Url del message: %s", nodemailer.getTestMessageUrl(info));
+      return res.status(200).send("Mail was sended");
     });
- });
- 
+  });
 };
 
-module.exports = { emailExample };
+module.exports = { sendEmail };
