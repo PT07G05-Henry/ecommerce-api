4ef21725fec9282076c6a477e492d5e95cb5efdd
FEDEIZ <ing.finterlandi@gmail.com>
2022-11-02 19:47:25 -0300

    Now we can update categories

--- src/routes/controllers/products/updateProduct.js
+++ src/routes/controllers/products/updateProduct.js
@@ -1,27 +1,24 @@
-const { Product } = require("../../../db");
+const { Product, Category } = require("../../../db");
 const { getProduct } = require("./getProductDetail");
 
 const updateProduct = async (req, res) => {
-  console.log(req.body)
-  const { id, name, price, description, stock, images } = req.body;
-  
-  try {
-    await Product.update(
-      {
-        name,
-        price: price && Number.parseFloat(price).toFixed(2), // convertir a float!!!
-        description, 
-        stock: stock && Number.parseInt(stock),
-        images, 
-      },
-      {
-        where: {
-          id: Number.parseInt(id),
-        },
-      }
-    );
+  console.log(req.body);
+  const { id, name, price, description, stock, images, categories } = req.body;
 
-    let productUpdate = await getProduct(id);
+  try {
+    const productDb = await getProduct(id);
+
+    await productDb.update({
+      name,
+      price: price && Number.parseFloat(price).toFixed(2), // convertir a float!!!
+      description,
+      stock: stock && Number.parseInt(stock),
+      images,
+    });
+
+    if (categories.length) await productDb.setCategories(categories);
+
+    const productUpdate = await getProduct(id);
     res.status(200);
     res.send(productUpdate);
   } catch (err) {
