e7e87735edb1e58a80984ac0059a987f516d2202
FEDEIZ <ing.finterlandi@gmail.com>
2022-11-02 23:19:03 -0300

    Middleware para control de stock antes de pagar

--- src/routes/mercadopag.js
+++ src/routes/mercadopag.js
@@ -1,14 +1,17 @@
 const { Router } = require("express");
-const { createLinkMP, notificacionorden } = require("./controllers/mercado/mercado.js");
+const {
+  createLinkMP,
+  notificacionorden,
+} = require("./controllers/mercado/mercado.js");
 const createOrder = require("./middlewares/createOrder.js");
+const checkStock = require("./middlewares/checkStock.js");
 
 const router = Router();
 
-
-router.post("/", createOrder, createLinkMP);
+router.post("/pay", checkStock, (req, res) => {
+  res.send("proceed to buy");
+});
+router.post("/", checkStock, createOrder, createLinkMP);
 router.get("/notificacion", notificacionorden);
-
-
-
 
 module.exports = router;
