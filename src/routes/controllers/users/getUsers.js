0ffb3a0201d64e3839e94e0090ba18c77696e275
FEDEIZ <ing.finterlandi@gmail.com>
2022-11-04 14:43:04 -0300

    getUsers by sid con endpoint /users?sid={sid}

--- src/routes/controllers/users/getUsers.js
+++ src/routes/controllers/users/getUsers.js
@@ -1,14 +1,19 @@
 const { User, Order, Product, Rol, Comment } = require("../../../db");
 
-const getUsersDb = async () => {
+const getUsersDb = async (sid) => {
+  if (sid)
+    User.findOne({
+      where: { sid },
+      attributes: ["id"],
+    });
   return User.findAll({
     include: [Order, Product, Rol, Comment],
     orderBy: [["id", "ASC"]],
   });
 };
-    
+
 const getUsers = async (req, res) => {
-  const email = req.query.email;
+  const { email, sid } = req.query;
   let users = await getUsersDb();
   if (email) {
     let findUser = await users.filter((u) =>
@@ -18,6 +23,10 @@ const getUsers = async (req, res) => {
       ? res.status(200).send(findUser)
       : res.status(404).send("There are no matches in the DB");
   } else {
+    if (sid) {
+      const user = await getUsersDb(sid);
+      return res.json(user.dataValues);
+    }
     if (users.length === 0)
       return res.send("There are no Users loaded in the DB");
     res.status(200).send(users);
