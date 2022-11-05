79d8d4ad8900c110bd4a234e3caa1278c9be9198
Profesorjand <jorge.andrade.y@hotmail.com>
2022-11-05 05:30:33 -0300

    modelo cart ajustes

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -6,63 +6,17 @@ const isImgUrl = require("../functions/isImgUrl")
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("cart", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      set(value) {
-        this.setDataValue("name", value.toUpperCase());
-      }
-    },
-    images: {
+    
+    items: {
         type: DataTypes.TEXT,
-        // allowNull: false,
         get: function () {
-          return JSON.parse(this.getDataValue("images"));
+          return JSON.parse(this.getDataValue("items"));
         },
         set: function (val) {
-          if (val.length === 0) {
-            return this.setDataValue(
-              "images",
-              JSON.stringify([
-                {
-                  image:
-                    "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg",
-                },
-              ])
-            );
-          }
-          return this.setDataValue("images", JSON.stringify(val));
+          return this.setDataValue("items", JSON.stringify(val));
         },
-        validate: {
-          async customValidator(value) {
-            for (const img in value) {
-              if (!(await isImgUrl(img.image))) {
-                throw new Error("Image url is broken");
-              }
-            }
-          },
-        },
-        defaultValue: JSON.stringify([
-          {
-            image:
-              "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg",
-          },
-        ]),
+        defaultValue: JSON.stringify([]),
       },
-    price: {
-        type: DataTypes.FLOAT
-    },
-    units: {
-        type: DataTypes.INTEGER
-    },
-    productId :{
-        type:DataTypes.INTEGER
-    },
     userId:{
         type: DataTypes.INTEGER
     },
