8a78934af1a14e727d19d14ac90ec026b4c0d54d
FEDEIZ <ing.finterlandi@gmail.com>
2022-11-06 14:41:26 -0300

    cloudinary aux function

---
+++ src/routes/auxFunctions/cloudinary.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
+
+const { CLOUD_NAME, API_KEY_CLOUDINARY, API_SECRET_CLOUDINARY } = process.env;
+
+cloudinary.config({
+  cloud_name: CLOUD_NAME,
+  api_key: API_KEY_CLOUDINARY,
+  api_secret: API_SECRET_CLOUDINARY,
+  secure: true,
+});
+
+const uploadImage = async function (filePath) {
+  return await cloudinary.uploader.upload(filePath, {
+    folder: "ecommerce",
+  });
+};
+
+// elimina archivo de la nube
+
+const deleteImageCloud = async function (publicId) {
+  return await cloudinary.uploader.destroy(publicId);
+};
+
+module.exports = { uploadImage, deleteImageCloud };
