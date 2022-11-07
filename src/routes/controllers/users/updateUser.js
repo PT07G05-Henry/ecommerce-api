e646239cf9cc30e671b2ce99926284198b6165b8
Profesorjand <jorge.andrade.y@hotmail.com>
2022-11-07 02:06:43 -0300

    set headers al back

--- src/routes/controllers/users/updateUser.js
+++ src/routes/controllers/users/updateUser.js
@@ -10,12 +10,19 @@ const getDetailUser = async (id) => {
 const updateUser = async (req, res) => {
   // actualizacion de datos de usuarios
   const { id, first_name, last_name, birth_date } = req.body;
+  console.log(req.files)
+  // console.log("id",id)
+  // console.log("first_name",first_name)
+  // console.log("last_name",last_name)
+  // console.log("birth_date",birth_date)
   try {
     let newProfile_picture = {};
     if (req.files?.profile_picture) {
       const cloudinaryImg = await uploadImage(
         req.files.profile_picture.tempFilePath
       );
+      console.log("secure url",cloudinaryImg.secure_url)
+      console.log("public id",cloudinaryImg.public_id)
       newProfile_picture = {
         secure_url: cloudinaryImg.secure_url,
         public_id: cloudinaryImg.public_id,
@@ -37,6 +44,7 @@ const updateUser = async (req, res) => {
       }
     );
     const result = await User.findByPk(Number.parseInt(id));
+    res.set("Content-Type", "multipart/form-data");
     res.status(200);
     res.send(result);
   } catch (err) {
