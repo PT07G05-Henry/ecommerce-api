4ea8b6615a822db87a15003c94ad844357611f35
FEDEIZ <ing.finterlandi@gmail.com>
2022-11-09 10:09:59 -0300

    categories.split ya es un array!

--- src/routes/controllers/products/updateProduct.js
+++ src/routes/controllers/products/updateProduct.js
@@ -37,7 +37,8 @@ const updateProduct = async (req, res) => {
       stock: stock && Number.parseInt(stock),
     });
 
-    const cat = [categories.split(",")].map((c) => Number.parseInt(c));
+    console.log(categories);
+    const cat = categories.split(",").map((c) => Number.parseInt(c));
 
     if (cat && cat.length) await productDb.setCategories(cat);
 
