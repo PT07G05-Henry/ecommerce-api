254dcf335ff50dd4653670490dc50ca6b961752c
Profesorjand <jorge.andrade.y@hotmail.com>
2022-11-09 04:26:33 -0300

    pasar Name por Query en Orders/All

--- src/routes/controllers/orders/getAllOrders.js
+++ src/routes/controllers/orders/getAllOrders.js
@@ -1,6 +1,12 @@
-const { Order, User, Delivery, Product, Payment } = require("../../../db");
+const { Order, User, Delivery, Product, Payment, Op } = require("../../../db");
 
-const getAllOrdersDb = async function (userId, status, order) {
+const getAllOrdersDb = async function (userId, status, order, name) {
+  const deco = decodeURIComponent(name)
+  const arr = deco.split(" ");
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+  const name2 = arr.join(" ");
   let orders = await Order.findAll({
     where: status
       ? {
@@ -13,6 +19,22 @@ const getAllOrdersDb = async function (userId, status, order) {
             model: User,
             where: { id: userId },
           },
+          {
+            model: Delivery,
+          },
+          {
+            model: Payment,
+          },
+        ]
+      : name
+      ? [
+          { model: User, where:  {[Op.or]: [
+            { first_name: {[Op.iLike]: `%${name2.split(" ")[0]}%` }} ,
+            { first_name: {[Op.iLike]: `%${name2.split(" ")[1]}%` }},
+            { last_name: {[Op.iLike]: `%${name2.split(" ")[0]}%` }},
+            { last_name: {[Op.iLike]: `%${name2.split(" ")[1]}%`} } 
+          ]} 
+        },
           {
             model: Delivery,
           },
@@ -35,9 +57,9 @@ const getAllOrdersDb = async function (userId, status, order) {
 };
 
 const getAllOrders = async (req, res) => {
-  const { userId, status, order } = req.query;
+  const { userId, status, order, name } = req.query;
   try {
-    let orders = await getAllOrdersDb(userId, status, order);
+    let orders = await getAllOrdersDb(userId, status, order, name);
     orders
       ? res.status(200).send(orders)
       : res.status(404).send({ error: "Order Not Found" });
