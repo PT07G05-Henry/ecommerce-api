814fadb1e5bb5de889a07ac7e554a1fc5179cb04
FEDEIZ <ing.finterlandi@gmail.com>
2022-11-10 13:39:56 -0300

    Ordenes en orden descendentes

--- src/routes/controllers/orders/getAllOrders.js
+++ src/routes/controllers/orders/getAllOrders.js
@@ -1,7 +1,7 @@
 const { Order, User, Delivery, Product, Payment, Op } = require("../../../db");
 
 const getAllOrdersDb = async function (userId, status, order, name) {
-  const deco = decodeURIComponent(name)
+  const deco = decodeURIComponent(name);
   const arr = deco.split(" ");
   for (var i = 0; i < arr.length; i++) {
     arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
@@ -28,13 +28,17 @@ const getAllOrdersDb = async function (userId, status, order, name) {
         ]
       : name
       ? [
-          { model: User, where:  {[Op.or]: [
-            { first_name: {[Op.iLike]: `%${name2.split(" ")[0]}%` }} ,
-            { first_name: {[Op.iLike]: `%${name2.split(" ")[1]}%` }},
-            { last_name: {[Op.iLike]: `%${name2.split(" ")[0]}%` }},
-            { last_name: {[Op.iLike]: `%${name2.split(" ")[1]}%`} } 
-          ]} 
-        },
+          {
+            model: User,
+            where: {
+              [Op.or]: [
+                { first_name: { [Op.iLike]: `%${name2.split(" ")[0]}%` } },
+                { first_name: { [Op.iLike]: `%${name2.split(" ")[1]}%` } },
+                { last_name: { [Op.iLike]: `%${name2.split(" ")[0]}%` } },
+                { last_name: { [Op.iLike]: `%${name2.split(" ")[1]}%` } },
+              ],
+            },
+          },
           {
             model: Delivery,
           },
@@ -51,7 +55,7 @@ const getAllOrdersDb = async function (userId, status, order, name) {
             model: Payment,
           },
         ],
-    order: order ? [["total_price", order]] : [["id", "asc"]],
+    order: order ? [["total_price", order]] : [["id", "desc"]],
   });
   return orders;
 };
