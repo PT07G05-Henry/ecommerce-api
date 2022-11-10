a5f18ddace61d07cbe5b5409a9610ac8976a87c8
Profesorjand <jorge.andrade.y@hotmail.com>
2022-11-10 13:46:11 -0300

    create Delivery falta celPhone

--- src/routes/controllers/deliveries/createDelivery.js
+++ src/routes/controllers/deliveries/createDelivery.js
@@ -1,7 +1,16 @@
 const { Delivery } = require("../../../db");
 
-const createDelivery = async (req, res) => {
+const createDelivery = async (req, res, next) => {
   //falta escribir acá
+ var dire= req.body.directionAddress;
+  try {
+    let newDelivery = await Delivery.create({ type:"Standard Shipping", status:"Pre-admission", shipping_address: dire.replace("Direction Address: ",""), phone_number:"1135098899" });
+    await newDelivery.setOrder(req.body.order);
+    next();
+  } catch (err) {
+    res.status(400);
+    res.send(err.message);
+  }
 };
 
 module.exports = { createDelivery };
